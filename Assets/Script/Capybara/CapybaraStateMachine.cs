c5b21c083d21989c40fccd2792f08927193f1dd2
Fatma Erdoğan <erdoganfatma119@gmail.com>
2025-07-30 02:39:02 +0300

    Update Enviroment & Added Capybara Animation State

---
+++ Assets/Script/Capybara/CapybaraStateMachine.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapybaraStateMachine : MonoBehaviour
+{
+    public CapybaraIdleState ýdleState { get; set; }
+    public CapybaraNormalSitState normalSitState { get; set; }
+    public CapybaraFatSitState fatSitState { get; set; }
+    public CapybaraChildSitState childSitState { get; set; }
+    public CapybaraWalkState walkState { get; set; }
+    public CapybaraRunState runState { get; set; }
+    public CapybaraSleepState sleepState { get; set; }
+    public CapybaraFreezeState freezeState { get; set; }
+
+    /// <summary>
+    /// Machine reference
+    /// </summary>
+    [field: SerializeField]
+    protected StateMachine Machine { get; private set; }
+    [field: SerializeField]
+    public CapybaraAnimation WAnimation { get; set; }
+
+    public Animator _animator;
+    public void FonksiyonStart()
+    {
+        WAnimation = new CapybaraAnimation(_animator);
+        // Set Machine
+        Machine = new StateMachine();
+        // Create states
+        CreateStates();
+
+    }
+    public void CreateStates()
+    {
+        ýdleState = new CapybaraIdleState(this);
+        normalSitState = new CapybaraNormalSitState(this);
+        fatSitState = new CapybaraFatSitState(this);
+        childSitState = new CapybaraChildSitState(this);
+        walkState = new CapybaraWalkState(this);
+        runState = new CapybaraRunState(this);
+        sleepState = new CapybaraSleepState(this);
+        freezeState = new CapybaraFreezeState(this);
+    }
+    public void SetState(CapybaraBaseState state)
+    {
+        Machine.SetState(state);
+    }
+}
